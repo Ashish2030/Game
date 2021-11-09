e6ba2dfe2a277962e0849110c25bffdbdc2767e1
1811981075 <kumarashish79924@gmail.com>
2021-11-09 14:01:26 +0530

    aaaa

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Game
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.initialize_on_precompile = false
+    config.sass.preferred_syntax = :scss
+    config.sass.line_comments = false
+    config.sass.cache = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
